594681cdd56d7d2311080f71bd1d0423a02560fb
justaway <eastaveracruz@gmail.com>
2018-06-26 22:53:21 +0300

    2

---
+++ src/main/java/lessons/Даты_календари_и_время/Даты_календари_и_время_Java.java
@@ -0,0 +1,25 @@
+package lessons.Даты_календари_и_время;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class Даты_календари_и_время_Java {
+    public static void main(String[] args) throws ParseException {
+        Date date = new Date(0);
+        System.out.println(date);
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.add(Calendar.WEEK_OF_MONTH, 1);
+        System.out.println(calendar.getTime());
+        DateFormat dateFormat = DateFormat.getDateInstance(DateFormat.SHORT);
+        System.out.println(dateFormat.format(calendar.getTime()));
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        System.out.println(simpleDateFormat.format(calendar.getTime()));
+
+        Date newDate = simpleDateFormat.parse("26/01/1992");
+        System.out.println(newDate);
+    }
+}
